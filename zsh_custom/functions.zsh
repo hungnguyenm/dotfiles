8ab332f38698256d17a24b424dd857e567fd2510
Hung Nguyen <hungng@seas.upenn.edu>
2017-01-16 22:43:02 -0500

    Updated function

--- zsh_custom/functions.zsh
+++ zsh_custom/functions.zsh
@@ -96,6 +96,7 @@ function ssh() {
 
 function ssh-dotfiles() {
   ssh -t "$@" "sudo apt-get install -y git;rm -rf ~/dotfiles;git clone https://github.com/hungnguyenm/dotfiles ~/dotfiles"
+  ssh "$@"
 }
 
 function ssh-tunnel() {
