c5fdcacb88292aec4b588e934a4ede435e82e44d
Hung Nguyen <hungng@seas.upenn.edu>
2017-06-02 01:17:41 -0400

    Added docker aliases

--- zsh_custom/func_docker.zsh
+++ zsh_custom/func_docker.zsh
@@ -1,4 +1,47 @@
-dk-cleanup(){
+# get latest container ID
+alias dkl="docker ps -l -q"
+
+# get container process
+alias dkps="docker ps"
+
+# get process included stop container
+alias dkpa="docker ps -a"
+
+# get images
+alias dki="docker images"
+
+# get container IP
+alias dkip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+
+# run deamonized container, e.g., $dkd base /bin/echo hello
+alias dkd="docker run -d -P"
+
+# Run interactive container, e.g., $dki base /bin/bash
+alias dkit="docker run -i -t -P"
+
+# execute interactive container, e.g., $dex base /bin/bash
+alias dkex="docker exec -i -t"
+
+# stop all containers
+dkstop() { docker stop $(docker ps -a -q); }
+
+# remove all containers
+dkrm() { docker rm $(docker ps -a -q); }
+
+# stop and remove all containers
+alias dkrmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+# remove all images
+dkrmi() { docker rmi $(docker images -q); }
+
+# show all alias related docker
+dkalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
+
+# bash into running container
+dkbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+
+# remove all container if possible, exited volumes, and dangling images
+dkclean(){
   local containers
   containers=( $(docker ps -aq 2>/dev/null) )
   docker rm "${containers[@]}" 2>/dev/null
@@ -9,3 +52,5 @@ dk-cleanup(){
   images=( $(docker images --filter dangling=true -q 2>/dev/null) )
   docker rmi "${images[@]}" 2>/dev/null
 }
+
+compctl -k "($__docker_complete_containers_names)" dkbash
