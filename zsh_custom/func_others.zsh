ab53912cacd37f2bdee99599360ce6fa4ddd87e5
Hung Nguyen <hungng@seas.upenn.edu>
2017-06-04 01:29:33 -0400

    Added fail-safe sudo commads

--- zsh_custom/func_others.zsh
+++ zsh_custom/func_others.zsh
@@ -3,6 +3,30 @@ function xcopy() { xsel --clipboard < "$*"; }
 function xover() { xsel --clipboard > "$*"; }
 function xpaste() { xsel --clipboard >> "$*"; }
 
+# fail-safe sudo commands
+alias shutdown="f_sudo_shutdown"
+alias f_sudo_shutdown="sudo shutdown"
+f_sudo_shutdown() {
+  read -q "_confirm? Do you want to shutdown $HOST [yn]? "
+  if [[ "$_confirm" =~ ^[Yy]$ ]]; then
+    sudo shutdown now
+  fi
+}
+
+alias reboot="sudo reboot"
+alias f_sudo_reboot="sudo reboot"
+f_sudo_reboot() {
+  read -q "_confirm? Do you want to reboot $HOST [yn]? "
+  if [[ "$_confirm" =~ ^[Yy]$ ]]; then
+    sudo reboot
+  fi
+}
+
+alias f_sudo_rmrf="sudo rm -rf /"
+f_sudo_rmrf() {
+  echo "No No No!!!"
+}
+
 # backup to private repository
 function backup-backup() {
   git_clone_private
