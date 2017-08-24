12753f96e7b65b95aabd4d2558b675c5b031f45d
Hung Nguyen <hungng@seas.upenn.edu>
2017-08-24 14:41:09 -0400

    Fixed bugs in conda plugin

--- zsh_custom/plugins/conda/conda.plugin.zsh
+++ zsh_custom/plugins/conda/conda.plugin.zsh
@@ -1,4 +1,4 @@
-CONDA_HOME=$(conda info | grep root | awk 'BEGIN { FS = " " } ; { print $4 }' | sed 's/^[ \t]*//;s/[ \t]*$//')
+CONDA_HOME=$(command -v conda >/dev/null 2>&1 && conda info | grep root | awk 'BEGIN { FS = " " } ; { print $4 }' | sed 's/^[ \t]*//;s/[ \t]*$//')
 
 _conda_list_envs() {
   compadd $(ls $CONDA_HOME/envs)
