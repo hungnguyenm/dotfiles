682845f0d4a947c911e46d666f130c4aafa17220
Hung Nguyen <hungng@seas.upenn.edu>
2017-12-09 17:52:50 -0500

    Added conda commands

--- zsh_custom/plugins/conda/conda.plugin.zsh
+++ zsh_custom/plugins/conda/conda.plugin.zsh
@@ -4,10 +4,18 @@ _conda_list_envs() {
   compadd $(ls $CONDA_HOME/envs)
 }
 
+worknew() {
+  conda create --name $1
+}
+
+workclean() {
+  conda remove --all --name $1
+}
+
 workon() {
   source activate $1
 }
-compdef _conda_list_envs workon
+compdef _conda_list_envs workclean workon
 
 workoff() {
   source deactivate
