b32612e2d4e738b0ec38ff5015838fbe1ce40f70
Hung Nguyen <hungng@seas.upenn.edu>
2018-08-07 00:45:00 -0400

    Added enotify function

--- zsh_custom/func.zsh
+++ zsh_custom/func.zsh
@@ -66,3 +66,44 @@ function pmv() {
         }
         END { print "" }' total_size=$(stat -c '%s' "${1}") count=0
 }
+
+# send email notify after a command finishes
+function enotify() {
+  if [ -n "$NOTIFY_USER" ] && [ -n "$NOTIFY_PASS" ] && [ -n "$NOTIFY_RCPT" ]; then
+    if [ -n "$PREFER_HOST_NAME" ]; then
+      name=$PREFER_HOST_NAME
+    else
+      name=$(hostname)
+    fi
+
+    { output=$((time $@)|tee /dev/fd/5); } 5>&1
+
+    if [ $? -eq 0 ]; then
+      msg="Command \"$@\" finished!"
+    else
+      msg="Command \"$@\" failed!"
+    fi
+    from="From:$name <$NOTIFY_USER>\n"
+    to="To:$NOTIFY_RCPT\n"
+    subject="Subject:[$name] $msg\n"
+    prefix="MIME-Version: 1.0\nContent-Type: text/html\nContent-Disposition: inline\n<html>\n<body>\n<pre style=\"font: monospace\">\n"
+    suffix="</pre></body></html>"
+    msg="$msg\n\n=== Output ===\n$output"
+
+    curl --ssl-reqd --silent --output /dev/null --connect-timeout 15 \
+      --url "smtps://smtp.gmail.com:465" \
+      --mail-from "$NOTIFY_USER" \
+      --mail-rcpt "$NOTIFY_RCPT" \
+      --user "$NOTIFY_USER:$NOTIFY_PASS" \
+      --upload-file =(echo "$from$to$subject$prefix$msg$suffix")
+
+    if [ $? -eq 0 ]; then
+      echo "  Notification email sent!"
+    else
+      echo "  Failed to send notification email"
+    fi
+
+  else
+    echo "fatal: login information is not set"
+  fi
+}
